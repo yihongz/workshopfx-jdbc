c9a236061a3648f0fbc2f2c157a73b147f610684
Paulo Almeida <estpaulovictora@live.com>
2021-06-13 11:32:16 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,12 +3,10 @@ package model.entities;
 import java.io.Serializable;
 
 public class Department implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
@@ -60,6 +58,6 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
+		return "Departments [id=" + id + ", name=" + name + "]";
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshopfxjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
