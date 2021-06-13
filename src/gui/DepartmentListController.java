70663b5726251478d150788e804ba7f88dcd07db
Paulo Almeida <estpaulovictora@live.com>
2021-06-13 18:19:26 -0300

    DepartmentFormController

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -55,7 +55,6 @@ public class DepartmentListController implements Initializable {
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
 		initializeNodes();
 	}
 	
