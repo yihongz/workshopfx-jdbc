bbf8bc8ecf735d88352f984b9a3b792f07559923
Paulo Almeida <estpaulovictora@live.com>
2021-06-13 10:06:40 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,10 @@
 module workshopfxjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+	opens model.services to javafx.graphics, javafx.fxml, javafx.base;
 }
